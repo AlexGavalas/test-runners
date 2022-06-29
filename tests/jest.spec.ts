2875f5a7d334ed9d9bf71ce37d0aef37e5440cb1
AlexGavalas <alex.gavalas.92@gmail.com>
2022-06-29 18:18:21 +0300

    first commit

---
+++ tests/jest.spec.ts
@@ -0,0 +1,7 @@
+describe('Jest Suite', () => {
+    it('contains spec with an expectation', () => {
+        expect(true).toBe(true);
+    });
+});
+
+export {};
---
+++ tests/vitest.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Vitest Suite', () => {
+    it('contains spec with an expectation', () => {
+        expect(true).toBe(true);
+    });
+});
