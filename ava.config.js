7682852d6feafccdcf7247eed5699369df98fa79
Alex Gavalas <alex.gavalas@workable.com>
2022-12-18 15:05:12 +0200

    feat: test react with mocha

---
+++ ava.config.js
@@ -0,0 +1,11 @@
+export default {
+    extensions: {
+        ts: 'module',
+        tsx: 'module',
+    },
+    nodeArguments: [
+        '--no-warnings',
+        '--loader=ts-node/esm',
+        '--experimental-specifier-resolution=node',
+    ],
+};
