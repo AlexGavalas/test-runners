e8a5524b81eae965cddfbb55c0c71387ec11163c
Alex Gavalas <alex.gavalas@workable.com>
2023-02-06 22:55:47 +0200

    refactor: refactors mocha tests

--- tests/mocha.tsx
+++ tests/mocha.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import { App } from '../src/app';
 import assert from 'node:assert';
-import { TEST_RUNS } from '../config';
+import { render, screen } from '@testing-library/react';
 
-const sum = (a: number, b: number) => a + b;
+import { App } from '../src/app';
+import { sum } from '../src/util';
+import { TEST_RUNS } from '../config';
 
 for (let i = 0; i < TEST_RUNS; i++) {
     it(`sum ${i}`, () => {
