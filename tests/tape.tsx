0fad627e55d0b1ca87296e5d8986ecaf15e923fc
Alex Gavalas <alex.gavalas@workable.com>
2023-02-06 23:42:03 +0200

    feat: adds tape

--- tests/tape.tsx
+++ tests/tape.tsx
@@ -1,18 +1,19 @@
 import { cleanup, render, screen } from '@testing-library/react';
-import test from 'ava';
+import test from 'tape';
 
 import { App } from '../src/app';
 import { sum } from '../src/util';
 import { TEST_RUNS } from '../config';
 
 for (let i = 0; i < TEST_RUNS; i++) {
-    test.afterEach(cleanup);
-
     test(`sum ${i}`, (t) => {
-        t.is(sum(1, 2), 3);
+        t.plan(1);
+        t.equal(sum(1, 2), 3);
     });
 
-    test.serial(`<App /> ${i}`, (t) => {
+    test(`<App /> ${i}`, (t) => {
+        t.plan(1);
+        cleanup();
         render(<App />);
         const button = screen.getByRole('button');
         t.is(button.textContent, 'Count is: 0');
